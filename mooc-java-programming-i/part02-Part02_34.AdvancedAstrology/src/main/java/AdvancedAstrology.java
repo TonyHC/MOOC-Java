8ae04cccf8054f1e4c7b10d5ab389b7360ea156a
Tony Chao <tchao@mail.usf.edu>
2021-08-13 21:00:36 -0400

    Make the code more clean and concise

--- mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
+++ mooc-java-programming-i/part02-Part02_34.AdvancedAstrology/src/main/java/AdvancedAstrology.java
@@ -1,50 +1,35 @@
 
 public class AdvancedAstrology {
-
-    public static void printStars(int number) {
-        // part 1 of the exercise
-        for(int i = 0; i < number; i++) {
-            System.out.print("*");
-        }
-        
-        System.out.println("");
+    public static void main(String[] args) {
+        christmasTree(10);
     }
 
     public static void printSpaces(int number) {
-        // part 1 of the exercise
-        for(int i = 1; i <= number; i++) {
+        for (int i = 1; i <= number; i++) {
             System.out.print(" ");
         }
     }
 
+    public static void printStars(int number) {
+        for (int i = 1; i <= number + (number - 1); i++) {
+            System.out.print("*");
+        }
+    }
+
     public static void printTriangle(int size) {
-        // part 2 of the exercise
-        for(int i = 1; i <= size; i++) {
+        for (int i = 1; i <= size; i++) {
             printSpaces(size - i);
             printStars(i);
+            System.out.println();
         }
     }
 
     public static void christmasTree(int height) {
-        // part 3 of the exercise
-        for(int i = 0; i < height; i++) {
-            printSpaces(height - (1 * i + 1));
-            printStars(2 * i + 1);
-        }
-        
-        for(int i = 0; i < 2; i++) {
-            printSpaces((height - 2));
-            printStars(3);
-        }
-    }
-
-    public static void main(String[] args) {
-        // The tests are not checking the main, so you can modify it freely.
-
-        printTriangle(4);
-        System.out.println("---");
-        christmasTree(4);
-        System.out.println("---");
-        christmasTree(10);
+        printTriangle(height);
+        printSpaces(height - 2);
+        printStars(2);
+        System.out.println();
+        printSpaces(height - 2);
+        printStars(2);
     }
 }
