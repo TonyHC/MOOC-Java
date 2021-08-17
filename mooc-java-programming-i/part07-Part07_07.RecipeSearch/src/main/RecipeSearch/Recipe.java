769b9a9959875f8c93f3d63e4e0cbf7131370cc9
Tony Chao <tchao@mail.usf.edu>
2021-08-17 11:45:58 -0400

    Contains information about a recipe

---
+++ mooc-java-programming-i/part07-Part07_07.RecipeSearch/src/main/RecipeSearch/Recipe.java
@@ -0,0 +1,36 @@
+package RecipeSearch;
+
+import java.util.ArrayList;
+
+public class Recipe {
+    private String name;
+    private int cookingTime;
+    private ArrayList<String> ingredients;
+
+    public Recipe(String name, int cookingTime) {
+        this.name = name;
+        this.cookingTime = cookingTime;
+        this.ingredients = new ArrayList<>();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getCookingTime() {
+        return cookingTime;
+    }
+
+    public ArrayList<String> getIngredients() {
+        return ingredients;
+    }
+
+    public void addIngredient(String name) {
+        this.ingredients.add(name);
+    }
+
+    @Override
+    public String toString() {
+        return name + ", cooking time: " + cookingTime;
+    }
+}
