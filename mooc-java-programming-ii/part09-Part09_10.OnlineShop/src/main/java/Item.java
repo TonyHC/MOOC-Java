ea37556796635394284413263ca01960c3d43ed5
Tony Chao <tchao@mail.usf.edu>
2021-08-18 21:30:13 -0400

    Fix the code format

--- mooc-java-programming-ii/part09-Part09_10.OnlineShop/src/main/java/Item.java
+++ mooc-java-programming-ii/part09-Part09_10.OnlineShop/src/main/java/Item.java
@@ -1,24 +1,24 @@
-
 public class Item {
     private String product;
-    private int qty;
+    private int quantity;
     private int unitPrice;
-    
-    public Item(String product, int qty, int unitPrice) {
+
+    public Item(String product, int quantity, int unitPrice) {
         this.product = product;
-        this.qty = qty;
+        this.quantity = quantity;
         this.unitPrice = unitPrice;
     }
-    
+
     public int price() {
-        return this.qty * this.unitPrice;
+        return this.quantity * this.unitPrice;
     }
-    
+
     public void increaseQuantity() {
-        this.qty += 1;
+        this.quantity += 1;
     }
-    
+
+    @Override
     public String toString() {
-        return product + ": " + qty;
+        return product + ": " + quantity;
     }
 }
