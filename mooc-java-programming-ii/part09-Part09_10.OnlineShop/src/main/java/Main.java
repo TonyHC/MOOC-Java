0b74d95fb7a6421273009846a8305be31771bcbb
Tony Chao <tchao@mail.usf.edu>
2021-08-18 21:31:40 -0400

    Added more products to Warhouse to select from

--- mooc-java-programming-ii/part09-Part09_10.OnlineShop/src/main/java/Main.java
+++ mooc-java-programming-ii/part09-Part09_10.OnlineShop/src/main/java/Main.java
@@ -1,17 +1,16 @@
-
 import java.util.Scanner;
 
 public class Main {
-
     public static void main(String[] args) {
         Warehouse warehouse = new Warehouse();
-        warehouse.addProduct("coffee", 5, 10);
-        warehouse.addProduct("milk", 3, 20);
-        warehouse.addProduct("cream", 2, 55);
-        warehouse.addProduct("bread", 7, 1);
+        warehouse.addProduct("Coffee", 5, 10);
+        warehouse.addProduct("Milk", 3, 20);
+        warehouse.addProduct("Cream", 2, 55);
+        warehouse.addProduct("Bread", 7, 8);
+        warehouse.addProduct("Bagel", 4, 50);
+        warehouse.addProduct("Donut", 2, 30);
 
         Scanner scanner = new Scanner(System.in);
-
         Store store = new Store(warehouse, scanner);
         store.shop("John");
     }
