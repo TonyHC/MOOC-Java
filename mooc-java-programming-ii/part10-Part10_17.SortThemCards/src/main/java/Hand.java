f3337d9a90ea3e1225b079fcf9fac273f88e86aa
Tony Chao <tchao@mail.usf.edu>
2021-08-20 11:47:31 -0400

    Added some new functions and updated sorting functions

--- mooc-java-programming-ii/part10-Part10_17.SortThemCards/src/main/java/Hand.java
+++ mooc-java-programming-ii/part10-Part10_17.SortThemCards/src/main/java/Hand.java
@@ -1,54 +1,91 @@
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.Comparator;
-import java.util.List;
-
+import java.util.*;
 
 public class Hand implements Comparable<Hand> {
-    private List<Card> cardList;
-    
+    private List<Card> cards;
+
     public Hand() {
-        this.cardList = new ArrayList<>();
+        this.cards = new ArrayList<>();
     }
-    
+
     public void add(Card card) {
-        this.cardList.add(card);
+        this.cards.add(card);
     }
-    
+
+    public void checkCards (int cardValue) {
+        Iterator<Card> iterator = cards.iterator();
+
+        while (iterator.hasNext()) {
+            Card nextCard = iterator.next();
+            if (nextCard.getValue() == cardValue)
+                System.out.println(nextCard);
+        }
+    }
+
+    public void checkCards (Suit suit) {
+        Iterator<Card> iterator = cards.iterator();
+
+        while (iterator.hasNext()) {
+            Card nextCard = iterator.next();
+            if (nextCard.getSuit() == suit)
+                System.out.println(nextCard);
+        }
+    }
+
+    public void removeCards(int cardValue) {
+        Iterator<Card> iterator = cards.iterator();
+
+        while (iterator.hasNext()) {
+            if (iterator.next().getValue() < cardValue)
+                iterator.remove();
+        }
+    }
+
+    public void removeCards(Suit suit) {
+        Iterator<Card> iterator = cards.iterator();
+
+        while (iterator.hasNext()) {
+            if (iterator.next().getSuit() == suit)
+                iterator.remove();
+        }
+    }
+
     public void print() {
-        this.cardList.stream().forEach(card -> System.out.println(card));
+        Iterator<Card> iterator = cards.iterator();
+
+        while (iterator.hasNext()) {
+            Card nextCard = iterator.next();
+            System.out.println(nextCard);
+        }
     }
-    
+
+    public int totalCardValues() {
+        int cardValues = 0;
+        for (Card card : cards)
+            cardValues += card.getValue();
+        return cardValues;
+    }
+
     public void sort() {
-        Collections.sort(cardList);
+        Collections.sort(cards);
+    }
+
+    public void sortBySuit() {
+        Collections.sort(cards, (c1,c2) -> c1.getSuit().ordinal() - c2.getSuit().ordinal());
+    }
+
+    public void sortBySuitInValueOrder() {
+        // Collections.sort(cards, new BySuitInValueOrder());
+        Collections.sort(cards, (c1,c2) -> {
+            if (c1.getSuit().ordinal() == c2.getSuit().ordinal()) {
+                return c1.getValue() - c2.getValue();
+            }
+
+            return c1.getSuit().ordinal() - c2.getSuit().ordinal();
+        });
     }
 
     @Override
     public int compareTo(Hand hand) {
-        if(this.totalCardsValue() == hand.totalCardsValue()) {
-            return 0;
-        }
-        
-        if(this.totalCardsValue() > hand.totalCardsValue()) {
-            return 1;
-        }
-        
-        return -1;
-    }
-    
-    public int totalCardsValue() {
-        int sum = this.cardList.stream()
-                .map(card -> card.getValue())
-                .reduce(0, (previous, current) -> previous + current);
-        
-        return sum;
-    }
-    
-    public void sortBySuit() {
-        Comparator<Card> comparator = Comparator.comparing(Card::getSuit)
-                                                .thenComparing(Card::getValue);
-        
-        Collections.sort(cardList, comparator);
+        return this.totalCardValues() - hand.totalCardValues();
     }
 }
