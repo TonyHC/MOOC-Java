d132c7f1467ea0ec68af148291dd6bbc361ab843
Tony Chao <tchao@mail.usf.edu>
2021-08-20 11:46:53 -0400

    New code to test new functions added in Hand class

--- mooc-java-programming-ii/part10-Part10_17.SortThemCards/src/main/java/Main.java
+++ mooc-java-programming-ii/part10-Part10_17.SortThemCards/src/main/java/Main.java
@@ -1,25 +1,5 @@
-
-import java.util.ArrayList;
-import java.util.Collections;
-
-
-
 public class Main {
-
     public static void main(String[] args) {
-        ArrayList<Card> cardList = new ArrayList<>();
-        cardList.add(new Card(3, Suit.SPADE));
-        cardList.add(new Card(2, Suit.DIAMOND));
-        cardList.add(new Card(14, Suit.SPADE));
-        cardList.add(new Card(12, Suit.HEART));
-        cardList.add(new Card(2, Suit.SPADE));
-        
-        Collections.sort(cardList, new BySuitInValueOrder());
-        
-        cardList.stream().forEach(card -> System.out.println(card));
-        
-        System.out.println("--------");
-        
         Hand hand = new Hand();
 
         hand.add(new Card(12, Suit.HEART));
@@ -29,8 +9,9 @@ public class Main {
         hand.add(new Card(7, Suit.HEART));
         hand.add(new Card(2, Suit.SPADE));
 
-        hand.sortBySuit();
-
+        hand.sort();
+        hand.checkCards(Suit.HEART);
+        hand.removeCards(3);
         hand.print();
     }
 }
